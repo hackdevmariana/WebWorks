980bbf72680d296c93ad538148ec0b67c13162c8
fernando <fernando@juandemariana.org>
2024-10-04 23:31:24 +0200

    Add HeroController

---
+++ packages/works/webworks/src/Controllers/HeroController.php
@@ -0,0 +1,21 @@
+<?php
+
+class HeroController extends Controller
+{
+    public function index($websiteName)
+    {
+        $heros = Content::where('website_id', $websiteName)
+                        ->where('content_type', 'hero')
+                        ->get();
+        return response()->json($heros);
+    }
+
+    public function show($websiteName, $heroItemName)
+    {
+        $heroItem = Content::where('website_id', $websiteName)
+                           ->where('name', $heroItemName)
+                           ->where('content_type', 'hero')
+                           ->firstOrFail();
+        return response()->json($heroItem);
+    }
+}
--- packages/works/webworks/src/Migrations/2024_10_04_200351_create_contents_table.php
+++ packages/works/webworks/src/Migrations/2024_10_04_200351_create_contents_table.php
@@ -7,22 +7,25 @@ class CreateContentsTable extends Migration
 {
     public function up()
     {
-        Schema::create('contents', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('website_id')->constrained('websites');
-            $table->string('name');
-            $table->string('title')->nullable();
-            $table->string('subtitle')->nullable();
-            $table->text('text')->nullable();
-            $table->string('image')->nullable();
-            $table->string('url')->nullable();
-            $table->string('alt')->nullable();
-            $table->string('content_type');
-            $table->boolean('is_default')->default(false);
-            $table->boolean('draft')->default(false);
-            $table->foreignId('author_id')->nullable()->constrained('authors');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('contents')) {
+            Schema::create('contents', function (Blueprint $table) {
+
+                $table->id();
+                $table->foreignId('website_id')->constrained('websites');
+                $table->string('name');
+                $table->string('title')->nullable();
+                $table->string('subtitle')->nullable();
+                $table->text('text')->nullable();
+                $table->string('image')->nullable();
+                $table->string('url')->nullable();
+                $table->string('alt')->nullable();
+                $table->string('content_type');
+                $table->boolean('is_default')->default(false);
+                $table->boolean('draft')->default(false);
+                $table->foreignId('author_id')->nullable()->constrained('authors');
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
