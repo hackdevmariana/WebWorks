f656641562b0e025d61d474bef60104988a4889c
fernando <fernando@juandemariana.org>
2024-10-04 23:44:52 +0200

    Add PostsController

---
+++ packages/works/webworks/src/Controllers/Api/PostsController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Works\Webworks\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Works\Webworks\Models\Content;
+use Works\Webworks\Models\Website;
+
+class PostsController extends Controller
+{
+    public function index($websiteName, Request $request)
+    {
+        $website = Website::where('name', $websiteName)->firstOrFail();
+        
+        $query = Content::where('website_id', $website->id)
+                        ->where('content_type', 'post');
+        
+        if ($request->has('date')) {
+            $query->whereDate('created_at', $request->date);
+        }
+        
+        if ($request->has('start') && $request->has('end')) {
+            $query->whereBetween('created_at', [$request->start, $request->end]);
+        }
+
+        $posts = $query->paginate($request->get('perPage', 10));
+
+        return response()->json($posts);
+    }
+
+    public function show($websiteName, $slug)
+    {
+        $website = Website::where('name', $websiteName)->firstOrFail();
+
+        $postItem = Content::where('website_id', $website->id)
+                           ->where('name', $slug)
+                           ->where('content_type', 'post')
+                           ->firstOrFail();
+                           
+        return response()->json($postItem);
+    }
+
+    public function filterByTag($websiteName, $tag, Request $request)
+    {
+        $website = Website::where('name', $websiteName)->firstOrFail();
+
+        $posts = Content::where('website_id', $website->id)
+                       ->where('content_type', 'post')
+                       ->where('tags', 'LIKE', "%$tag%")
+                       ->paginate($request->get('perPage', 10));
+
+        return response()->json($posts);
+    }
+}
